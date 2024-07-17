424557f88fbc9b6920e7775696de2e92c898988d
Motyak <m0tyak_@live.fr>
2024-07-18 00:08:26 +0200

    looks good

--- s.py
+++ s.py
@@ -1,5 +1,8 @@
+import functools
 import itertools
 import sys
+
+B64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 def slurp_as_bytes(file):
     with open(file, encoding="utf-8") as f:
@@ -13,6 +16,43 @@ def utf8_to_bytes(input):
     assert isinstance(input, str)
     return input.encode("utf-8")
 
+def byte_to_bin_str(byte):
+    return bin(byte)[2:].rjust(8, "0")
+
+def sextet_to_bin_str(sextet):
+    return bin(sextet)[2:].rjust(6, "0")
+
+def bin_str_to_int(bin_str):
+    return int(bin_str, 2)
+
+def b64encode(input) -> str:
+    global B64_ALPHABET
+    assert isinstance(B64_ALPHABET, str)
+    assert len(B64_ALPHABET) == 64
+    if len(input) == 0:
+        return ""
+    if isinstance(input, str):
+        input = utf8_to_bytes(input)
+
+    reducer = lambda acc, c: acc + byte_to_bin_str(c)
+    bin_str = functools.reduce(reducer, input, "")
+    sextets = [bin_str[i:i+6] for i in range(0, len(bin_str), 6)]
+    sextets[-1] = sextets[-1].ljust(6, "0") # potential padding
+    indices = [*map(bin_str_to_int, sextets)]
+    base64 = "".join([*map(lambda i: B64_ALPHABET[i], indices)])
+    return base64
+
+def b64decode(input) -> bytes:
+    global B64_ALPHABET
+    assert isinstance(B64_ALPHABET, str)
+    assert len(B64_ALPHABET) == 64
+    indices = [*map(lambda c: B64_ALPHABET.index(c), input)]
+    sextets = [*map(lambda i: sextet_to_bin_str(i), indices)]
+    bin_str = "".join(sextets)
+    if len(bin_str) % 8 != 0:
+        bin_str = bin_str[:-(len(bin_str) % 8)] # drop useless bits
+    return bytes(int(bin_str[i:i+8], 2) for i in range(0, len(bin_str), 8))
+
 def xor(secret, input) -> bytes:
     if isinstance(secret, str):
         secret = utf8_to_bytes(secret)
@@ -23,7 +63,7 @@ def xor(secret, input) -> bytes:
     pairs = zip(iterator_secret, input)
     return bytes(map(lambda x: x[0] ^ x[1], pairs))
 
-def hashfn(input) -> bytes:
+def hashfn(input) -> str:
     if isinstance(input, str):
         input = utf8_to_bytes(input)
 
@@ -31,7 +71,7 @@ def hashfn(input) -> bytes:
         assert isinstance(hash, int)
         assert 0 <= hash and hash <= 255
         str_hash = hex(43210 + int(hash * (22222 / 255)))[2:]
-        return utf8_to_bytes(str_hash)
+        return str_hash
     
     T = [i for i in range(0, 256)]
     hash = 0
@@ -39,20 +79,20 @@ def hashfn(input) -> bytes:
         hash = T[hash ^ b]
     return md5ify(hash)
 
-def encrypt(secret, input) -> bytes:
-    return hashfn(input) + b'\n' + xor(secret, input)
+def encrypt(secret, input) -> str:
+    return hashfn(input) + b64encode(xor(secret, input))
 
 def decrypt(secret, input_str) -> bytes:
     if isinstance(input_str, bytes):
         input_str = bytes_to_utf8(input_str)
-    inputs = input_str.split("\n", 1)
-    if len(inputs) != 2:
+    if len(input_str) < 4:
+        raise Exception("invalid input")       
+    hash = input_str[0:4]
+    decoded_payload = b64decode(input_str[4:])
+    decrypted = xor(secret, decoded_payload)
+    if hashfn(decrypted) != hash:
         raise Exception("invalid input")
-    hash, payload = inputs
-    decrypted_payload = xor(secret, payload)
-    if hashfn(decrypted_payload) != utf8_to_bytes(hash):
-        raise Exception("invalid input")
-    return decrypted_payload
+    return decrypted
 
 if __name__ == "__main__":
     secret = slurp_as_bytes(__file__)
@@ -61,8 +101,8 @@ if __name__ == "__main__":
     print(input)
 
     encrypted = encrypt(secret, input)
-    # encrypted = b'd2a8\n\x0e\t\x03e' # will not work => invalid input
-    # encrypted = b'fdsfds' # will not work either
+    # encrypted = "d2a8EwkDZQ" # will not work => invalid input
+    # encrypted = "fdsfds" # will not work either
     print(encrypted)
 
     decrypted = decrypt(secret, encrypted)
