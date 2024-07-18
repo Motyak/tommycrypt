bc08653ca6e550ca01a86435689ec9c3ba8609fa
Tommy Anfosso <anfossotommy@hotmail.fr>
2024-07-18 22:01:20 +0200

    yay

---
+++ unepage.php
@@ -0,0 +1,42 @@
+<?php
+    function encrypt($msg) {
+        return `/bin/bash -c 'python3 encrypt.py <<< "{$msg}"'`;
+    }
+
+    function decrypt($cipher) {
+        return `/bin/bash -c 'python3 decrypt.py <<< "{$cipher}"'`;
+    }
+
+    /* handle POST */
+    if (isset($_POST["encrypt"])) {
+        echo encrypt($_POST["encrypt"]);
+        exit(0);
+    }
+    if (isset($_POST["decrypt"])) {
+        echo decrypt($_POST["decrypt"]);
+        exit(0);
+    }
+?>
+
+<!-- encrypt -->
+<form method="GET">
+    <input name="encrypt" type="text" placeholder="&lt;message&gt;" />
+    <input type="submit" value="ENCRYPT" />
+</form>
+
+<!-- decrypt -->
+<form method="GET">
+    <input name="decrypt" type="text" placeholder="&lt;cipher&gt;" />
+    <input type="submit" value="DECRYPT" />
+</form>
+
+<?php
+    /* handle GET */
+    if (isset($_GET["encrypt"])) {
+        echo "Your cipher is: " . encrypt($_GET["encrypt"]);
+        exit(0);
+    }
+    if (isset($_GET["decrypt"])) {
+        echo "Your message is: " . decrypt($_GET["decrypt"]);
+    }
+?>
